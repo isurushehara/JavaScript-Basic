415c9173c1f8039172064658f11765b868b76e4b
isuru0 <isurushehara@gmail.com>
2025-04-02 20:39:03 +0530

    Add static and object assign lesson in classes files

--- 17 Classes/class.js
+++ 17 Classes/class.js
@@ -19,12 +19,43 @@ isuru.showInfo();
 
 //INHERITANCE
 let inheritanceResult = document.getElementById('inheritanceResult');
+let staticResult = document.getElementById('staticResult');
 
 class Student extends Person {
+    //CHILD CONSTRUCTOR
+    constructor(name, schoolName) {
+        super(name, []);
+        this.schoolName = schoolName;
+    }
     showType() {
         inheritanceResult.innerHTML = "Student";
     }
+    
+    //STATIC [THIS CAN CALL EVEN DON'T CREATE A OBJECT]
+    static show() {
+        staticResult.innerHTML = "Static";
+    }
 }
 
-const shehara = new Student("Shehara", 23);
+Student.show();
+
+const shehara = new Student("Shehara", "JNS");
 shehara.showType();
+
+//OBJECT ASSIGN
+let objectAssignResult = document.getElementById('objectAssignResult');
+
+const object_1 = {
+    name: "Object 1",
+    type: 1
+}
+const object_2 = {
+    objectName() {
+        objectAssignResult.innerHTML = "Object Name: " +this.name;
+    },
+    type: 2
+}
+
+Object.assign(object_1, object_2);
+
+object_1.objectName();
