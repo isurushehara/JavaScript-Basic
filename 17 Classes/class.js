61c03bb3899d3c8f3669967e2aefab4f0a308dbc
isuru0 <isurushehara@gmail.com>
2025-04-02 20:06:21 +0530

    Add inheritances lesson in classes files

--- 17 Classes/class.js
+++ 17 Classes/class.js
@@ -9,7 +9,22 @@ class Person {
     showInfo() {
         classResult.innerHTML = "I am " +this.name +" and " +this.age +" years old.";
     }
+    showType() {
+        classResult.innerHTML = "Person";
+    }
 }
 
 const isuru = new Person("Isuru", 23);
 isuru.showInfo();
+
+//INHERITANCE
+let inheritanceResult = document.getElementById('inheritanceResult');
+
+class Student extends Person {
+    showType() {
+        inheritanceResult.innerHTML = "Student";
+    }
+}
+
+const shehara = new Student("Shehara", 23);
+shehara.showType();
