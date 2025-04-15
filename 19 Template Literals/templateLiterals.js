914412f7825e8115e641d32160d5fade5a9f44ad
isuru0 <isurushehara@gmail.com>
2025-04-15 12:01:48 +0530

    Add Template Literals lesson

---
+++ 19 Template Literals/templateLiterals.js
@@ -0,0 +1,8 @@
+let resultNormal = document.getElementById('resultNormal');
+let resultTL = document.getElementById('resultTL');
+
+let name = "Isuru";
+let type = "Student";
+
+resultNormal.innerHTML = "Name: " +name+ "<br> Type: " +type;
+resultTL.innerHTML = `Name: ${name} <br> Type: ${type}`;
